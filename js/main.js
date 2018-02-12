404636a2484314eedc1cabe9eb03c867d11823ea
Matvey Rukol <banano-palma@yandex.ru>
2018-02-13 00:45:34 +0300

    finally all sections

--- js/main.js
+++ js/main.js
@@ -30,5 +30,13 @@ var compositionInf = document.querySelector('#composition__info');
 
 
 compositionBtn.addEventListener('mouseover', function() {
+	compositionInf.style.padding = '13px 20px';
 	compositionInf.style.height = 'auto';
-})
+	compositionInf.style.opacity = '1';
+});
+
+compositionBtn.addEventListener('mouseout', function() {
+	compositionInf.style.padding = '0px 0px';
+	compositionInf.style.height = '0';
+	compositionInf.style.opacity = '0';
+});
