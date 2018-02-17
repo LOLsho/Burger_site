08635897c863b2f05b76a24f79dd11d6a6555ea2
Matvey Rukol <banano-palma@yandex.ru>
2018-02-17 22:55:46 +0300

    apdate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// --- Functions --- //
+function delay(ms) {
+	return new Promise(function(resolve) {
+		setTimeout(function() {
+			resolve();
+		}, ms);
+	});
+}
+
 // --->> team <<--- //
 var allBtns = document.querySelectorAll('.team__acco-item');
 var links = document.querySelectorAll('.team__acco-trigger');
@@ -39,4 +48,46 @@ compositionBtn.addEventListener('mouseout', function() {
 	compositionInf.style.padding = '0px 0px';
 	compositionInf.style.height = '0';
 	compositionInf.style.opacity = '0';
+});
+
+// --->> reviews <<--- //
+var overlay = document.querySelector('#overlay');
+var overlay__close = overlay.querySelector('.overlay__close');
+
+var reviews__btn1 = document.querySelector('#reviews__btn1');
+var reviews__btn2 = document.querySelector('#reviews__btn2');
+var reviews__btn3 = document.querySelector('#reviews__btn3');
+var reviews__btn4 = document.querySelector('#reviews__btn4');
+var reviews__btn5 = document.querySelector('#reviews__btn5');
+var reviews__btn6 = document.querySelector('#reviews__btn6');
+var reviews__btn7 = document.querySelector('#reviews__btn7');
+var reviews__btn8 = document.querySelector('#reviews__btn8');
+
+var reviews__btns = [reviews__btn1,
+					reviews__btn2,
+					reviews__btn3,
+					reviews__btn4,
+					reviews__btn5,
+					reviews__btn6,
+					reviews__btn7,
+					reviews__btn8];
+
+for (var i=0; i<reviews__btns.length; ++i) {
+	reviews__btns[i].addEventListener('click', function(event) {
+		
+		event.preventDefault();
+
+		overlay.style.display = 'flex';
+		
+		delay(1).then(function() {
+			overlay.style.opacity = '1';
+		});
+	});
+}
+
+overlay__close.addEventListener('click', function() {
+	overlay.style.opacity = '0';
+	delay(900).then(function() {
+		overlay.style.display = 'none';
+	});
 });
